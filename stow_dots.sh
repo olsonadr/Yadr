7c58608481f54caf8873982325eaabe0dc1c29a5
root <nick.ryan.olson@gmail.com>
2024-05-02 02:40:30 +0000

    Add script to run stow command(s)

---
+++ stow_dots.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# stow_dots.sh
+# Use GNU stow to symlink dotfiles from the "stow" subdirectory
+# into the parent directory of this script, passing any commandline
+# arguments directly to stow.
+
+function log() {
+  local -r prefix="~=: "
+  echo "${prefix}$@"
+}
+
+readonly script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+readonly stow_opts="--target=${script_dir}/.."
+readonly stow_command="stow ${stow_opts} $@ */"
+
+log "Entering: ${script_dir}/stow"
+cd ${script_dir}/stow
+
+log "Running: ${stow_command}"
+${stow_command}
+
+log "Leaving: ${script_dir}/stow"
+cd -
+
+exit 0
