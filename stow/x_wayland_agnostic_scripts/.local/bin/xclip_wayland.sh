8ae7f3ad15f170d3cc260194490bc9b01d6b1855
olsonadr <olsonnick10@yahoo.com>
2025-06-21 19:52:10 +0200

    Add skeletons of TODO scripts

---
+++ stow/x_wayland_agnostic_scripts/.local/bin/xclip_wayland.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# TODO: Wrap either xclip or wl-copy/wl-paste depending on X or Wayland running.
+#       Must support copy and paste for clipboard and "primary" buffers.
+
---
+++ stow/x_wayland_agnostic_scripts/.local/bin/xdotool_wayland.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# TODO: Wrap either xdotool or wlrctl depending on X or Wayland running
+
