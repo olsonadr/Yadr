23ce608c3ba63e42442ece4f450bea372478d28b
olsonadr <olsonnick10@yahoo.com>
2025-08-15 00:14:40 +0200

    Dashboard update in progress

---
+++ stow/lazyvim/.config/lazyvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+-- For customization of snacks plugin
+
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = "wow",
+        },
+      },
+    },
+  },
+}
